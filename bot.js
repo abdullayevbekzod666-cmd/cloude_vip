0bff4da13f5907ab67515a79275d618d7708b304
abdullayevbekzod666-cmd <abdullayevbekzod666@gmail.com>
2026-06-23 20:33:22 +0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,9 +2,9 @@ const { Telegraf, Markup } = require("telegraf");
 const express = require("express");
 const fs = require("fs");
 
-const bot = new Telegraf("SENING_TOKENING");
-
+const bot = new Telegraf("8135606663:AAHqwY3_SXSuIV8WBLMwwvorpP5sp2jhC2c");
 const app = express();
+
 let users = {};
 
 if (fs.existsSync("database.json")) {
@@ -18,7 +18,7 @@ function saveData() {
 bot.start((ctx) => {
     if (!users[ctx.from.id]) {
         users[ctx.from.id] = {
-            username: ctx.from.username,
+            username: ctx.from.username || "NoUsername",
             balance: 0,
             referrals: 0
         };
@@ -26,14 +26,26 @@ bot.start((ctx) => {
     }
 
     ctx.reply(
-        "CLOUDE VIP PREMIUM",
+        "🔥 CLOUDE VIP PREMIUM 🔥",
         Markup.inlineKeyboard([
-            [Markup.button.webApp("OPEN APP", "GITHUB_LINKING")]
+            [
+                Markup.button.webApp(
+                    "🚀 OPEN APP",
+                    "https://abdullayevbekzod666-cmd.github.io/cloude_vip/"
+                )
+            ]
         ])
     );
+});
+
+bot.on("photo", async (ctx) => {
+    await ctx.reply("✅ Screenshot qabul qilindi. Admin tekshiradi.");
 });
 
 bot.launch();
 
 app.use(express.static("public"));
-app.listen(3000);
+
+app.listen(3000, () => {
+    console.log("Server ishlayapti...");
+});
